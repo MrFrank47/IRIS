a56415417e7d0c368bd72d3ee6459be8ffd3593a
MrFrank47 <z7gzn8sqcz@privaterelay.appleid.com>
2025-11-27 10:15:59 +0100

    Initial Commit

---
+++ ProvaAI/ProvaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProvaAIApp.swift
+//  ProvaAI
+//
+//  Created by Francesco Lombardi on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ProvaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
